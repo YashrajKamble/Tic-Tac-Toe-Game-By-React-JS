d7ad7c53ed605103aee4eade780311dd8c545d9a
Yashraj_K <yashrajkamble7727@gamil.com>
2024-07-15 18:50:42 +0530

    positioning the board

--- src/Components/TicTacToe.jsx
+++ src/Components/TicTacToe.jsx
@@ -1,7 +1,35 @@
 import "./TicTacToe.css";
+import circle from "./Assets/circle.png";
+import cross from "./Assets/cross.png";
 
 function TicTacToe() {
-  return <div>TicTacToe</div>;
+  return (
+    <div className="container">
+      <h1 className="title">
+        Tic Tac Toe <span>React</span>
+      </h1>
+
+      <div className="board">
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+      </div>
+
+      <button className="reset">Reset</button>
+    </div>
+  );
 }
 
 export default TicTacToe;
