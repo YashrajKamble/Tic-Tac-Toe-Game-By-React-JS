bb4bc428c47a3e162b4d08a42e4ecc5c36dddac7
Yashraj_K <yashrajkamble7727@gamil.com>
2024-07-15 19:47:18 +0530

    up to useRef()

--- src/Components/TicTacToe.jsx
+++ src/Components/TicTacToe.jsx
@@ -1,8 +1,58 @@
 import "./TicTacToe.css";
 import circle from "./Assets/circle.png";
 import cross from "./Assets/cross.png";
+import { useState } from "react";
+import { useRef } from "react";
+
+let data = ["", "", "", "", "", "", "", "", ""];
 
 function TicTacToe() {
+  const [count, setCount] = useState();
+  const [lock, setLock] = useState(false);
+  const titleRef = useRef(null);
+
+  const toggle = (e, num) => {
+    if (lock) {
+      return 0;
+    }
+    if (count % 2 === 0) {
+      e.target.innerHTML = `<img src='${cross}'>`;
+      data[num] = "x";
+      setCount(++count);
+    } else {
+      e.target.innerHTML = `<img src='${circle}'>`;
+      data[num] = "o";
+      setCount(++count);
+    }
+    checkWin();
+  };
+
+  const checkWin = () => {
+    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data);
+    } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
+      won(data);
+    } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
+      won(data);
+    } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
+      won(data);
+    } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
+      won(data);
+    } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
+      won(data);
+    } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
+      won(data);
+    } else if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data);
+    } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
+      won(data);
+    }
+  };
+
+  const won = (winner) => {
+    setLock(true);
+  };
+
   return (
     <div className="container">
       <h1 className="title">
@@ -11,19 +61,66 @@ function TicTacToe() {
 
       <div className="board">
         <div className="row1">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 0);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 1);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 2);
+            }}
+          ></div>
         </div>
+
         <div className="row2">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 3);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 4);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 5);
+            }}
+          ></div>
         </div>
+
         <div className="row3">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 6);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 7);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 8);
+            }}
+          ></div>
         </div>
       </div>
 
